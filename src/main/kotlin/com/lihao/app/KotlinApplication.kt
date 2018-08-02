5cce5b0127800812fbf2e6b446e81ababac538f6
lihao8173 <lihao198262@163.com>
2018-08-02 15:12:05 +0800

    add static html

--- src/main/kotlin/com/lihao/app/KotlinApplication.kt
+++ src/main/kotlin/com/lihao/app/KotlinApplication.kt
@@ -1,11 +1,16 @@
 package com.lihao.app
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
+
+val logger = LoggerFactory.getLogger("KotlinApplication.kt")
 
 @SpringBootApplication
 open class KotlinApplication
@@ -21,10 +26,16 @@ class HelloController {
 @Controller
 class MainController {
     @GetMapping("login")
-    fun login(modle : Model): String {
+    fun login(modle: Model): String {
         val a = 100;
         modle.addAttribute("a", a);
         return "login.html"
+    }
+
+    @PostMapping("login-post")
+    fun loginCheck(username:String, password:String): String {
+        logger.info("aa $username-> $password")
+        return "index"
     }
 }
 
